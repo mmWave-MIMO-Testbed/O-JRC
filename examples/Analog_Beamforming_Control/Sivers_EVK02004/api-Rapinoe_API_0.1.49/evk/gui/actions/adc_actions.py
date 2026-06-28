a96fec8d814215f5e09207277f469603d9c35aa6
HaochengZhu <114951914+HaochengZhu@users.noreply.github.com>
2026-06-28 18:01:03 -0400

    Remove analog control and LLM part

--- examples/Analog_Beamforming_Control/Sivers_EVK02004/api-Rapinoe_API_0.1.49/evk/gui/actions/adc_actions.py
+++
@@ -1,7 +0,0 @@
-class AdcActions():
-
-    def __init__(self, host):
-        self._host = host
-
-    def enable(self, dev):
-        self._host.chip.adc.enable(dev)
--- examples/Analog_Beamforming_Control/Sivers_EVK02004/api-Rapinoe_API_0.1.49/evk2/gui/actions/adc_actions.py
+++
@@ -1,8 +0,0 @@
-class AdcActions():
-
-    def __init__(self, gui_data):
-        self._gui_data = gui_data
-
-    def enable(self):
-        command = "self._host.chip.adc.enable(__RAP__)"
-        self._gui_data.exec_command(command)
